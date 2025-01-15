b16d2120c6b5b4aa4bff91d2c99449f0162c4405
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2025-01-15 11:18:37 -0500

    add semantic cache to quickly shoot deer without hitting LLM

--- example_agent/utils/ex_semantic_cache.py
+++ example_agent/utils/ex_semantic_cache.py
@@ -11,9 +11,12 @@ REDIS_URL = os.environ.get("REDIS_URL", "redis://host.docker.internal:6379/0")
 hunting_example = "There's a deer. You're starving. You know what you have to do..."
 
 semantic_cache = SemanticCache(
-    name="oregon_trail_cache",
-    redis_url=REDIS_URL,
-    distance_threshold=0.1,
+  name="oregon_trail_cache",
+  redis_url=REDIS_URL,
+  distance_threshold=0.1,
 )
 
-semantic_cache.store(prompt=hunting_example, response="bang")
+semantic_cache.store(
+  prompt=hunting_example,
+  response="bang"
+)
